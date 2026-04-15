8aab4fb9db98292b7e1a3a9addf2bcf9c3a0d34d
Henry Brüning <henry.bruening@hsw-stud.de>
2026-04-15 09:20:09 +0200

    Initial commit

---
+++ src/main/java/org/quizz/politicianparty/PoliticianPartyApplication.java
@@ -0,0 +1,13 @@
+package org.quizz.politicianparty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoliticianPartyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoliticianPartyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/quizz/politicianparty/PoliticianPartyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.quizz.politicianparty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoliticianPartyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
